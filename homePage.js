b138b5c288b1f773e16d6813cf8f8723cf11cff3
Tony <taztony2010@yahoo.com>
2015-07-03 10:59:57 -0700

    First_Commit
    Added html/css/js

---
+++ homePage.js
@@ -0,0 +1,29 @@
+/**
+ * Created by tonyhendrick on 6/16/15.
+ */
+var userName = "";
+var password = "";
+
+function logIn(){
+    var userName = prompt("Username: ");
+    var password = prompt("Password: ");
+}
+
+var x;
+
+function changecolors() {
+    x = 1;
+    setInterval(change, 1000);
+}
+
+function change() {
+
+    var hex = ["red", "blue", "green", "orange", "yellow"];
+
+    var color = hex[Math.floor(Math.random() * 4)];
+
+    document.body.style.background = color;
+}
+
+$(document).ready(function() {
+});
