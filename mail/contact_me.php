0527d931186a615d30c8084688a754bfb6474062
Tony <taztony2010@yahoo.com>
2015-07-11 11:14:07 -0700

    Contact Form
    Changed the contact form from being PHP by default to now using
    Formspree

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,7 +2,6 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -17,7 +16,7 @@ $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'taztony2010@yahoo.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$email_subject = "Hatbkwds.github.io contact form submitted by:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@hatbkwds.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
